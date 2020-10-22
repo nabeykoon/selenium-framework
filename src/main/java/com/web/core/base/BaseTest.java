cdb8b26fedd693e81c7cd087372ad98596a7b41f
Nadeera Abeykoon <nabeykoon@gmail.com>
2020-10-23 02:02:51 +0530

    First commit with project files

---
+++ src/main/java/com/web/core/base/BaseTest.java
@@ -0,0 +1,81 @@
+package com.web.core.base;
+
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.TestResultsSummary;
+import com.applitools.eyes.selenium.ClassicRunner;
+import com.applitools.eyes.selenium.Eyes;
+import com.web.core.util.EyesManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.util.Properties;
+
+@Listeners({com.web.core.listeners.TestListener.class})
+
+
+public class BaseTest {
+    private static final ThreadLocal<WebDriver> drivers = new ThreadLocal<WebDriver>();
+    protected Logger log;
+    protected EyesManager eyesManager;
+
+    protected String testSuiteName;
+    protected String testName;
+    protected String testMethodName;
+
+    public WebDriver getDriver() {
+        return drivers.get();
+    }
+
+    @Parameters({"browser", "os", "node"})
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, @Optional("chrome") String browser, @Optional String os, @Optional String node, ITestContext context) {
+
+        //String testName = context.getCurrentXmlTest().getName();
+        String testName = method.getName ();
+        log = LogManager.getLogger(testName);
+
+        //context.setAttribute ("WebDriver", driver);
+        context.setAttribute("browser", browser);
+        this.testSuiteName = context.getSuite().getName();
+        this.testName = testName;
+        this.testMethodName = method.getName();
+
+        if (node != null) {
+            try {
+                RemoteWebDriver driver = BrowserDriverFactory.createRemoteDriver(browser, log, os, node);
+                driver.manage().window().maximize();
+                drivers.set(driver);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            WebDriver driver = BrowserDriverFactory.createDriver(browser, log);
+            driver.manage().window().maximize();
+            drivers.set(driver);
+        }
+
+        eyesManager = new EyesManager(getDriver(), "heroApp");
+
+    }
+
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        log.info("Close driver");
+        getDriver().quit();
+        drivers.remove();
+        eyesManager.abort();
+        eyesManager.generateResults ();
+    }
+}
---
+++ src/main/java/com/web/core/util/EyesManager.java
@@ -0,0 +1,76 @@
+package com.web.core.util;
+
+import com.applitools.eyes.*;
+import com.applitools.eyes.selenium.ClassicRunner;
+import com.applitools.eyes.selenium.Eyes;
+import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+
+public class EyesManager {
+
+    private Eyes eyes;
+    private EyesRunner runner;
+    private String appName;
+    private WebDriver driver;
+
+    public EyesManager(WebDriver driver, String appName) {
+        this.driver = driver;
+        this.appName = appName;
+        runner = new ClassicRunner();
+        eyes = new Eyes(runner);
+        eyes.setApiKey(PropertyUtils.getProperty("applitools.api.key"));
+    }
+
+    public void setBatchName(String batchName) {
+        eyes.setBatch(new BatchInfo(batchName));
+    }
+
+    public void abort() {
+        eyes.abortIfNotClosed();
+    }
+
+    public void generateResults() {
+        TestResultsSummary allTestResults = runner.getAllTestResults();
+    }
+
+    public void validateWindow(Enum Match, boolean requireFullPage) {
+        eyes.open(driver, "herokuapp", Thread.currentThread().getStackTrace()[2].getMethodName(), new RectangleSize(1920, 1080));
+        eyes.setMatchLevel(MatchLevel.STRICT);
+        //define Full page need to be considered
+        eyes.setForceFullPageScreenshot(requireFullPage);
+        eyes.checkWindow();
+        eyes.close();
+    }
+
+    public void validateElement(By locator) {
+        eyes.open(driver, "herokuapp", Thread.currentThread().getStackTrace()[2].getMethodName());
+        eyes.checkElement(locator);
+        eyes.close();
+    }
+
+    public void validateFrame(String locator) {
+        eyes.open(driver, "herokuapp", Thread.currentThread().getStackTrace()[2].getMethodName());
+        eyes.checkFrame(locator);
+        eyes.close();
+    }
+
+    public boolean validatePDF(String filepath) throws IOException, InterruptedException {
+        String command = String.format(
+                "java -jar src/main/resources/ImageTester.jar -k %s -f %s",
+                PropertyUtils.getProperty ("applitools.api.key"),
+                filepath);
+
+        Process process = Runtime.getRuntime().exec(command);
+        process.waitFor();
+        String stream = IOUtils.toString(process.getInputStream(), "UTF-8");
+        System.out.println(stream);
+
+        if(stream != null && stream.contains("Mismatch")){
+            return false;
+        }
+        return true;
+    }
+}
