867fab212164f4863bb5cd67779762b9a1ca8c90
Nadeera Abeykoon <nabeykoon@gmail.com>
2020-12-30 18:15:32 +0530

    added javascript execution method performJavascriptActionOnElement

--- src/main/java/com/web/core/base/BasePageObject.java
+++ src/main/java/com/web/core/base/BasePageObject.java
@@ -27,6 +27,7 @@ public class BasePageObject {
 
     /**
      * Open page with given url
+     * @param url
      */
     protected void openUrl(String url) {
         driver.get(url);
@@ -51,19 +52,26 @@ public class BasePageObject {
 
     /**
      * Find element using given locator
+     * @param locator
+     * @return
      */
 
     protected WebElement find(By locator) {
         return driver.findElement(locator);
     }
 
-    /** Find all elements using given locator */
+    /**
+     * Find all elements using given locator
+     * @param locator
+     * @return
+     */
     protected List<WebElement> findAll(By locator) {
         return driver.findElements(locator);
     }
 
     /**
      * Click on element with given locator when its visible
+     * @param locator
      */
     protected void click(By locator) {
         waitForVisibilityOf(locator, 10);
@@ -72,6 +80,7 @@ public class BasePageObject {
 
     /**
      * Focus on element and then click on it with given locator when its clickable
+     * @param locator
      */
 
     protected void focusedClick(By locator){
@@ -86,6 +95,7 @@ public class BasePageObject {
 
     /**
      * Click on slow loadable buttons (Buttons with synchronization issues)
+     * @param locator
      */
 
     protected void clickOnSlowLoadableButton(By locator){
@@ -95,7 +105,8 @@ public class BasePageObject {
     }
 
     /**
-     * Click on slow loadable buttons (Buttons with synchronization issues)
+     * Focus and click on slow loadable buttons (Buttons with synchronization issues)
+     * @param locator
      */
 
     protected void focusedClickOnSlowLoadableButton(By locator){
@@ -106,6 +117,8 @@ public class BasePageObject {
 
     /**
      * Type given text into element with given locator
+     * @param text
+     * @param locator
      */
     protected void type(String text, By locator) {
         waitForVisibilityOf(locator, 10);
@@ -125,6 +138,8 @@ public class BasePageObject {
 
     /**
      * Wait for specific ExpectedCondition for the given amount of time in seconds
+     * @param condition
+     * @param timeOutInSeconds
      */
     private void waitFor(ExpectedCondition<WebElement> condition, Integer timeOutInSeconds) {
         timeOutInSeconds = timeOutInSeconds != null ? timeOutInSeconds : 30;
@@ -133,8 +148,9 @@ public class BasePageObject {
     }
 
     /**
-     * Wait for given number of seconds for element with given locator to be visible
-     * on the page
+     * Wait for given number of seconds for element with given locator to be visible on the page
+     * @param locator
+     * @param timeOutInSeconds
      */
     protected void waitForVisibilityOf(By locator, Integer... timeOutInSeconds) {
         int attempts = 0;
@@ -151,7 +167,8 @@ public class BasePageObject {
 
     /**
      * Wait for given number of seconds for element with given locator until clickable
-     * on the page
+     * @param locator
+     * @param timeOutInSeconds
      */
     protected void waitUntilClickable(By locator, Integer... timeOutInSeconds) {
         int attempts = 0;
@@ -174,13 +191,20 @@ public class BasePageObject {
         ngDriver.waitForAngularRequestsToFinish ();
     }
 
-    /** Wait for alert present and then switch to it */
+    /**
+     * Wait for alert present and then switch to it
+     */
+
     protected Alert switchToAlert() {
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.alertIsPresent());
         return driver.switchTo().alert();
     }
 
+    /**
+     * Switch to another Tab
+     * @param expectedTitle
+     */
     public void switchToWindowWithTitle(String expectedTitle) {
         // Switching to new window
         String firstWindow = driver.getWindowHandle();
@@ -199,21 +223,36 @@ public class BasePageObject {
         }
     }
 
-    /** Switch to iFrame using it's locator */
+    /**
+     * Switch to iFrame using it's locator
+     * @param frameLocator
+     */
     protected void switchToFrame(By frameLocator) {
         driver.switchTo().frame(find(frameLocator));
     }
 
-    /** Press Key on locator */
+    /**
+     * Press Key on locator
+     * @param locator
+     * @param key
+     */
     protected void pressKey(By locator, Keys key) {
         find(locator).sendKeys(key);
     }
 
-    /** Press Key using Actions class */
+    /**
+     * Press Key using Actions class
+     * @param key
+     */
     public void pressKeyWithActions(Keys key) {
         log.info("Pressing " + key.name() + " using Actions class");
         Actions action = new Actions(driver);
         action.sendKeys(key).build().perform();
+    }
+
+    public void performJavascriptActionOnElement(By locator, String arg){
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript (arg, find (locator));
     }
 
     /** Perform scroll to the bottom */
@@ -223,7 +262,11 @@ public class BasePageObject {
         jsExecutor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
     }
 
-    /** Drag 'from' element to 'to' element */
+    /**
+     * Drag 'from' element to 'to' element
+     * @param from
+     * @param to
+     */
     protected void performDragAndDrop(By from, By to) {
 
         //Due to selenium bug following approach may not work in some pages
@@ -252,20 +295,30 @@ public class BasePageObject {
                 find(from), find(to));
     }
 
-    /** Perform mouse hover over element */
+    /**
+     * Perform mouse hover over element
+     * @param element
+     */
     protected void hoverOverElement(WebElement element) {
         Actions action = new Actions(driver);
         action.moveToElement(element).build().perform();
     }
 
-    /** Add cookie */
+    /**
+     * Add cookie
+     * @param ck
+     */
     public void setCookie(Cookie ck) {
         log.info("Adding cookie " + ck.getName());
         driver.manage().addCookie(ck);
         log.info("Cookie added");
     }
 
-    /** Get cookie value using cookie name */
+    /**
+     * Get cookie value using cookie name
+     * @param name
+     * @return
+     */
     public String getCookie(String name) {
         log.info("Getting value of cookie " + name);
         return driver.manage().getCookieNamed(name).getValue();
